08bccdcb09c22ff15378bac47032a866688f3bbe
Matt Ravenmoore <M_ravenmoore@outlook.com>
2020-08-12 19:24:13 -0700

    cleaning up branches and resubmits

--- js/app.js
+++ js/app.js
@@ -2,54 +2,6 @@
 // proof of life
 console.log('I AM A DRAGON AND I AM ALIVE')
 
-// city object litterals  used for first day of project COMMENTED OUT SO I CAN REMEMBER IT
-// function cityObjectLiterals(){
-//   var seattle = {
-//     location: 'seattle',
-//     minCustomers: 23,
-//     maxCustomers: 65,
-//     avgCookieBuy: 6.3,
-//     dailyCookieCount: [],
-//     guestCookiesArr: [],
-//     calcGuestsCookies: function(){
-//       var cookiesPerHourArr = [];
-//       var dailyCount = 0;
-//       var guestCount = [];
-//       // loops through store hours
-//       for(var i=0; i < storeHoursArr.length; i++){
-//         // attaches store hours to variable guestsPerHour
-//         generateGuestCount = Math.ceil(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers);
-//         guestCount.push(generateGuestCount);
-//         cookiesPerHour = Math.ceil(generateGuestCount * this.avgCookieBuy);
-//         cookiesPerHourArr.push(cookiesPerHour);
-//         dailyCount += cookiesPerHour;
-//         this.guestCookiesArr.push(`${storeHoursArr[i]} : ${cookiesPerHourArr[i]}`,)
-//       } 
-//       this.dailyCookieCount.push(dailyCount);
-//     },
-//     renderCity: function(){
-//       // put name on page
-//       var storeName = document.getElementById(`${this.location}`);
-//       storeName.textContent = this.location;
-
-//       var parentElement = document.getElementById(`${this.location}Data`);
-//       for(var i=0; i< this.guestCookiesArr[1].length; i++){
-//         var listItem = document.createElement('li');
-//         listItem.textContent = `${this.guestCookiesArr[i]} cookies`;
-//         parentElement.appendChild(listItem);
-//       }
-      
-//       var totalCookiesSold = document.getElementById(`${this.location}Total`);
-//       totalCookiesSold.textContent = `Total cookies ${this.dailyCookieCount}`
-//     }
-//   }
-//   seattle.calcGuestsCookies();
-//   console.log(seattle);
-//   seattle.renderCity();
-// }
-
-// Hours of opperation and locations
-
 var storeHoursArr = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm']
 var allStores = [];
 
@@ -101,7 +53,6 @@ function header(){
 
 
 new Stores('Seattle', 23,65,6.3);
-Stores.seattle;
 new Stores('Tokyo',3,24,1.2);
 new Stores('Dubai',11,38,3.7);
 new Stores('Paris',20,38,2.3);
