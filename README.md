34c88a83b731e3d630b3bfe4c11e6172fcc99cd1
Matt Ravenmoore <M_ravenmoore@outlook.com>
2020-08-16 16:09:56 -0700

    basic clean up and trying to fix minor bugs

--- README.md
+++ README.md
@@ -21,16 +21,31 @@ these are notes from the builder about current projects and ideas for future fea
 
 
 Html things
+
 - put in content
 - salmon image
+use the 7 images needed
+
 
 css things
-- get a base style set up
-- look at problem scope and find a good plan for color and lay out do sketch!!!!
 
-- lay out css and apply settings
+* find one sans serif font(data) one serif (text) and one Highlight font
+* 3 colors for fonts one for each font
+* 3 bg colors for elements
+* page background color
+
+* lay out css and apply settings
 
 Java script things
+
+fixes:
+
+* table header needs to go to 7pm
+* need to add row header of locations
+* add totals row
+* add totals colmn
+
+
 
 * future parent function allTheSalmonCookies to hold and runn as one command
 * future function runCookies
--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
-'useStrict'
+'useStrict';
 // proof of life
-console.log('I AM A DRAGON AND I AM ALIVE')
+console.log('I AM A DRAGON AND I AM ALIVE');
 
-// Hours of opperation and locations
+// temp for posible feature
+// var storeLocationsArr = [seattle,tokyo,dubai,paris,lima];
 
-var storeHoursArr = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm']
-var storeLocationsArr = ['Seattle','Tokyo','Dubai','Paris','Lima']
+// Global Variables
+var storeHoursArr = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 var allStores = [];
-var parentElement = document.getElementById("table");
+var parentElement = document.getElementById('table');
+
 
 // lets make some Objects with a constructor
-function Location(locationName,minCustomers,maxCustomers,avgCookieBuy){
+function StoreLocation(locationName,minCustomers,maxCustomers,avgCookieBuy){
   this.location = locationName;
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
@@ -19,71 +21,65 @@ function Location(locationName,minCustomers,maxCustomers,avgCookieBuy){
   this.guestCookiesArr = [];
   allStores.push(this);
 }
-var seattle = new Location('Seattle', 23,65,6.3);
-var tokyo = new Location('Tokyo',3,24,1.2);
-var dubai = new Location('Dubai',11,38,3.7);
-var paris = new Location('Paris',20,38,2.3);
-var lima = new Location('Lima',2,16,4.6);
+
 
 // Prototypes for Location
 
 //get guests per hour
-Location.prototype.calcGuestCookies = function(){
+StoreLocation.prototype.calcGuestCookies = function(){
   var cookiesPerHourArr = [];
   var dailyCount = 0;
   var guestCount = [];
   // loops through store hours
   for(var i=0; i < storeHoursArr.length; i++){
     // attaches store hours to variable guestsPerHour
-    generateGuestCount = getRandom(this.minCustomers,this.maxCustomers)
+    var generateGuestCount = getRandom(this.minCustomers,this.maxCustomers);
     guestCount.push(generateGuestCount);
-    cookiesPerHour = Math.ceil(generateGuestCount * this.avgCookieBuy);
+    var cookiesPerHour = Math.ceil(generateGuestCount * this.avgCookieBuy);
     cookiesPerHourArr.push(cookiesPerHour);
     dailyCount += cookiesPerHour;
-    this.guestCookiesArr.push([storeHoursArr[i], cookiesPerHourArr[i]])
-  } 
+    this.guestCookiesArr.push([storeHoursArr[i], cookiesPerHourArr[i]]);
+  }
   this.dailyCookieCount.push(dailyCount);
-} 
+};
 
 // make row of data from cookie hour total and hours of the day
-Location.prototype.renderData = function(){
+StoreLocation.prototype.renderData = function(){
   var tableRow = document.createElement('tr');
   parentElement.appendChild(tableRow);
   var cookiearrayholder = this.guestCookiesArr;
-  
   for(var i=0;i<cookiearrayholder.length;i++) {
     var tableData = document.createElement('td');
-    tableData.textContent = cookiearrayholder[i][1]
+    tableData.textContent = cookiearrayholder[i][1];
     tableRow.appendChild(tableData);
   }
-}
+};
+var seattle = new StoreLocation('Seattle', 23,65,6.3);
+var tokyo = new StoreLocation('Tokyo',3,24,1.2);
+var dubai = new StoreLocation('Dubai',11,38,3.7);
+var paris = new StoreLocation('Paris',20,38,2.3);
+var lima = new StoreLocation('Lima',2,16,4.6);
 
 // helper functions
 function getRandom(minCust,maxCust){
   return Math.ceil(Math.random() * (maxCust - minCust) + minCust);
 }
 
-function buildHeader(){
+function buildChart(){
   var tableRow = document.createElement('tr');
-  parentElement.appendChild(tableRow);
-  
-
-  for(var i=0;i <storeHoursArr.length; i++){
-    var tableHead = document.createElement('th')
-    tableHead.textContent = `${storeHoursArr[i]}`; 
-    tableRow.appendChild(tableHead);
+  for(var i=0 ;i < storeHoursArr.length; i++){
+    var tableHead = document.createElement('th');
+    tableHead.textContent = `${storeHoursArr[i]}`;
+    console.log(allStores[i]);
     allStores[i].calcGuestCookies();
     allStores[i].renderData();
+    tableRow.appendChild(tableHead);
   }
+  parentElement.appendChild(tableRow);
 }
 
-
-
-
-
-
 function buildStoreContent(){
-  buildHeader();
+  buildChart();
 }
 
 // PUT IT ON PAGE
